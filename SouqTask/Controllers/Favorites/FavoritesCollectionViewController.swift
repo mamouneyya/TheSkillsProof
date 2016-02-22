b96e7451bd49bf60aad78096c499281e0ca61603
Ma'moun Diraneyya <mamoun.diraneyya@gmail.com>
2016-02-22 23:27:45 +0200

    Use collection view instead of table view for favorites controller, for more consistency with the products controller.

--- SouqTask/Controllers/Favorites/FavoritesCollectionViewController.swift
+++ SouqTask/Controllers/Favorites/FavoritesCollectionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FavoritesTableViewController: BaseTableViewController {
+class FavoritesCollectionViewController: BaseCollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
