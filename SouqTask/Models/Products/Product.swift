3cd77099eeb9d65f72540fff83b19632ee45e476
Ma'moun Diraneyya <mamoun.diraneyya@gmail.com>
2016-02-24 01:34:07 +0200

    Product model is now conforming to Mappable protocol, for quick and easy JSON parsing.

--- SouqTask/Models/Products/Product.swift
+++ SouqTask/Models/Products/Product.swift
@@ -7,26 +7,58 @@
 //
 
 import UIKit
+import ObjectMapper
 
 class Price {
+    
+    // MARK: Vars
+    
     var value = 0
     var currency = "$"
+    
+    // MARK: Lifecycle
+    
+    init() {
+        
+    }
     
     init(_ value: Int) {
         self.value = value
     }
 }
 
-class Product {
+class Product: Mappable {
 
+    // MARK: Vars
+    
     var id = ""
+    var categoryId = ""
     var title = ""
     
-    var price = Price(0)
+    var price = Price()
     var prices = [Price]()
 
     var imageURL: NSURL?
     
     var favorited = false
+
+    // MARK: Lifecycle
     
+    init() {
+
+    }
+    
+    required init?(_ map: Map) {
+        
+    }
+    
+    // MARK: Mappable
+    
+    func mapping(map: Map) {
+        id          <- map["id"]
+        categoryId  <- map["product_type_id"]
+        title       <- map["label"]
+        price       <- map["offer_price"]
+        imageURL    <- (map["images.L.0"], URLTransform())
+    }
 }
