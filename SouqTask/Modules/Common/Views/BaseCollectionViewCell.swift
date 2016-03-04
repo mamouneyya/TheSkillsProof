f0f5789d77b2628baf16712beee0fcc7d08aedd4
Ma'moun Diraneyya <mamoun.diraneyya@gmail.com>
2016-03-04 22:06:12 +0200

    Add Base classes for UITableViewCell and UICollectionViewCell. Even if we currently do nothing in them, it's still a better structure.

---
+++ SouqTask/Modules/Common/Views/BaseCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BaseCollectionViewCell.swift
+//  SouqTask
+//
+//  Created by Ma'moun Diraneyya on 3/4/16.
+//  Copyright © 2016 Mamouneyya. All rights reserved.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ SouqTask/Modules/Common/Views/BaseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BaseTableViewCell.swift
+//  SouqTask
+//
+//  Created by Ma'moun Diraneyya on 3/4/16.
+//  Copyright © 2016 Mamouneyya. All rights reserved.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
