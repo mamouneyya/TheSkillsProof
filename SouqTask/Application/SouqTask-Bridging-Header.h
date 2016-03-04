70143e0f9e65543b4fef71a67bacc53ae25f57e2
Ma'moun Diraneyya <mamoun.diraneyya@gmail.com>
2016-03-04 21:39:47 +0200

    Create a bridging header so we can import important libraries implicitly.

---
+++ SouqTask/Application/SouqTask-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SouqTask-Bridging-Header.h
+//  SouqTask
+//
+//  Created by Ma'moun Diraneyya on 3/4/16.
+//  Copyright © 2016 Mamouneyya. All rights reserved.
+//
+//  NOTE This workaround is used to import important libraries everywhere in the app
+//  REF http://stackoverflow.com/questions/26408612/implicitly-import-specific-swift-module
+//
+
+@import Alamofire;
+@import AlamofireImage;
+@import ObjectMapper;
+@import SwiftyUserDefaults;
