ddcac52e93192eb5ba599cae598a1b3a74c2a81e
Ma'moun Diraneyya <mamoun.diraneyya@gmail.com>
2016-03-05 22:19:41 +0200

    Fix some wrong directory paths.

---
+++ SouqTask/Modules/Prices/Helpers/PricesManager.swift
@@ -0,0 +1,13 @@
+//
+//  PricesManager.swift
+//  SouqTask
+//
+//  Created by Ma'moun Diraneyya on 3/5/16.
+//  Copyright © 2016 Mamouneyya. All rights reserved.
+//
+
+import UIKit
+
+class PricesManager: NSObject {
+
+}
